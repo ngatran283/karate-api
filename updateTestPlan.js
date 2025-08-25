84a19db33be4607755afd2b05a9b77f7e416b4d0
ngatran <>
2025-08-26 03:46:40 +0700

    test

--- updateTestPlan.js
+++ updateTestPlan.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
-import path from 'path';
-import AdmZip from 'adm-zip';
-import fetch from 'node-fetch';
-import { parseStringPromise } from 'xml2js';
+const fs = require('fs');
+const path = require('path');
+const AdmZip = require('adm-zip');
+const fetch = require('node-fetch');
+const { parseStringPromise } = require('xml2js');
 
 // Environment variables from pipeline
 const ADO_ORG = process.env.ADO_ORG;
